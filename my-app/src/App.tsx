8e591e98eacf094d1d7122b235722c99f80efa5b
waldmaier <waldmaier@localhost>
2024-10-16 22:34:23 +0200

    buttons external

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import DialogComponent from "./dialog/DialogComponent";
 
 function App() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  console.log("app render " + isOpen)
   return (
     <div className="App">
-      <DialogComponent id="example-dialog">
+      <button onClick={() => setIsOpen(true)}>Open Dialog in App</button>
+
+      <DialogComponent id="example-dialog" open={isOpen}>
         <h3>Custom Content Inside Modal</h3>
         <p>This is some dynamic content passed to the modal.</p>
+        <button onClick={() => setIsOpen(false)}>Close</button>
       </DialogComponent>
     </div>
   );
--- my-app/src/dialog/DialogComponent.tsx
+++ my-app/src/dialog/DialogComponent.tsx
@@ -1,38 +1,24 @@
-import React, { useState, ReactNode } from 'react';
+import React, {ReactNode, useEffect, useState} from 'react';
 
 // Define the type for props, including children
 type DialogProps = {
   id: string;
   children: ReactNode;  // This allows any React content to be passed as children
+  open: boolean;
 };
 
-const DialogComponent = ({ id, children }: DialogProps) => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-
-  const openDialog = (): void => {
-    setIsOpen(true);
-  };
-
-  const closeDialog = (): void => {
-    setIsOpen(false);
-  };
-
+const DialogComponent = ({id, children, open}: DialogProps) => {
   return (
     <div>
       <h2>React Dialog Example</h2>
-      <button onClick={openDialog}>Open Dialog</button>
-
       {/* Render the dialog only if isOpen is true */}
-      {isOpen && (
+      {open && (
         <>
-          <div className="dialog-backdrop" onClick={closeDialog} />
-          <dialog id={id} open aria-hidden={!isOpen}>
+          <div className="dialog-backdrop" /*onClick={closeDialog}*//>
+          <dialog id={id} open aria-hidden={!open}>
             <div>
               {children} {/* Render the passed content here */}
             </div>
-            <button onClick={closeDialog} autoFocus>
-              Close
-            </button>
           </dialog>
         </>
       )}
